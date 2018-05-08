ddc1e85513451348d909bae40332d6d4dbe26a3e
Majid Nisar <majidnisar@gmail.com>
2018-05-09 01:31:32 +0530

    :rocket: Express Simple REST

--- index.js
+++ index.js
@@ -1,10 +1,51 @@
 const express = require('express')
 const http = require('http')
 const morgan = require('morgan')
+const bodyParser = require('body-parser')
 const hostname = 'localhost'
 const port = 3000
 const app = express();
 app.use(morgan('dev'))
+app.use(bodyParser.json())
+//general middleware
+app.all('/dishes', (req, res, next) => {
+  res.statusCode = 200
+  res.setHeader('Content-Type', 'text/plain')
+  next()
+});
+app.get('/dishes', (req, res, next) => {
+  res.end('Hi there! will send all the dishes for your request')
+});
+app.post('/dishes', (req, res, next) => {
+  res.end(`Thanks! for your valuable input will add the dish: ${req.body.name} with the details : ${req.body.description}`)
+});
+app.put('/dishes', (req, res, next) => {
+  res.statusCode = 403; //operation not supported 
+  res.end(`Thanks! PUT Operation is not supported on /dishes`)
+});
+//dangerous operationa and only authenticated users to be allowed this one
+app.delete('/dishes', (req, res, next) => {
+  res.end('Deleting all the dishes')
+});
+
+//on a particular id
+
+app.get('/dishes/:dishID', (req, res, next) => {
+  res.end(`Hi there! will send you details of dish :${req.params.dishID} for your request `)
+});
+app.post('/dishes/:dishID', (req, res, next) => {
+  res.statusCode = 403; //operation not supported 
+  res.end(`Thanks! POST Operation is not supported on /dishes/${req.params.dishID}`)
+});
+app.put('/dishes/:dishID', (req, res, next) => {
+  console.log(req)
+  res.end(`Thanks! will update the dish ${req.params.dishID} with Dish name:${req.body.name} and description ${req.body.description}`)
+});
+//dangerous operationa and only authenticated users to be allowed this one
+app.delete('/dishes/:dishID', (req, res, next) => {
+  res.end(`Deleting dish ${req.params.dishID}`)
+});
+
 app.use(express.static(__dirname + '/public'))
 app.use((req, res, next) => {
   // console.log(req.headers)
